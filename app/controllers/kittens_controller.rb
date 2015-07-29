f562de246116db77d718fb7de82cc0a634c34237
Judd Nutting <juddnutting@gmail.com>
2015-07-28 21:28:33 -0600

    working html and json

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -3,10 +3,19 @@ class KittensController < ApplicationController
 	def index
 		@kittens = Kitten.all
 
+		respond_to do |format|
+			format.html
+			format.json { render :json => @kittens} 
+		end
+
 	end
 
 	def show
 		@kitten = Kitten.find(params[:id])
+		respond_to do |format|
+			format.html
+			format.json {render :json => @kitten}
+		end
 	end
 
 	def new
@@ -22,6 +31,27 @@ class KittensController < ApplicationController
 		else
 			render 'new'
 		end
+	end
+
+	def edit
+		@kitten = Kitten.find(params[:id])
+		@range_options = (1..10)
+	end
+
+	def update
+		@kitten = Kitten.find(params[:id])
+		if @kitten.update(kitten_params)
+			flash[:succes] = "Kitten updated"
+			redirect_to @kitten
+		else
+		redirect_to root_url
+		end
+	end
+
+	def destroy
+		Kitten.find(params[:id]).destroy
+		flash[:succes] = "Kitten deleted"
+		redirect_to root_url
 	end
 
 	private
